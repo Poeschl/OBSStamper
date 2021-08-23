defad90534d7131c476c06f5064ac1be9bf7e6a1
Markus Pöschl <Poeschl@users.noreply.github.com>
2021-08-23 21:25:30 +0200

    :sparkles: Implement start and first connection to OBS

--- src/main/kotlin/xyz/poeschl/tools/obsstamper/Application.kt
+++ src/main/kotlin/xyz/poeschl/tools/obsstamper/Application.kt
@@ -1,12 +1,18 @@
 package xyz.poeschl.tools.obsstamper
 
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
 import mu.KotlinLogging
+import net.twasi.obsremotejava.OBSRemoteController
+import java.nio.file.Path
 
-fun main() {
-    ObsStamper().start()
+fun main(args: Array<String>) {
+    ArgParser(args).parseInto(::Args).run {
+        ObsStamper(host, Path.of(parentFolder), websocketPassword).start()
+    }
 }
 
-class ObsStamper {
+class ObsStamper(private val host: String, private val parentFolder: Path, private val websocketPassword: String?) {
 
     companion object {
         val LOGGER = KotlinLogging.logger { }
@@ -14,5 +20,22 @@ class ObsStamper {
 
     fun start() {
         LOGGER.info { "Started ObsStamper" }
+
+        val obsController = when {
+            (websocketPassword != null && websocketPassword.isNotBlank()) -> OBSRemoteController(host, false, websocketPassword)
+            else -> OBSRemoteController(host, false)
+        }
+
+        if (obsController.isFailed) {
+            LOGGER.error { "OBS Connection failed" }
+        } else {
+            // Do stuff via websockets
+        }
     }
+}
+
+class Args(parser: ArgParser) {
+    val host by parser.storing("-host", help = "The host where OBS is running. Defaults to 'ws://localhost:4444'.").default("ws://localhost:4444")
+    val parentFolder by parser.storing("--f", "--folder", help = "The folder where the textfiles are generated. Defaults to the current directory").default(".")
+    val websocketPassword by parser.storing("--p", "--password", help = "The password for the OBS websocket")
 }
